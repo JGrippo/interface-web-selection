3d52ff2a3035147242a14ac2764aecf23796f19a
YouKnow232 <chaseharland@bellsouth.net>
2018-06-06 16:43:21 -0400

    Added room and address model classes, added mock room data

---
+++ src/app/models/address.ts
@@ -0,0 +1,10 @@
+// Subject to change
+
+export class Address {
+  Street: string;
+  aptNum: string;
+  City: string;
+  State: string;
+  PostalCode: string;
+  Country: string;
+}
---
+++ src/app/models/room.ts
@@ -0,0 +1,11 @@
+// Subject to change
+import { Address } from "./address";
+
+export class Room {
+  RoomId: string;
+  Location: string;
+  Address: Address;
+  Vacancy: number;
+  Occupancy: number;
+  Gender: string;
+}
