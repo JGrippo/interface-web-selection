16fee0bea508c87e0352ce3dac84caabd78cca81
Ram Adhikari <ramadhikari96@gmail.com>
2018-06-06 15:17:31 -0400

    Added user model inside models folder

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,20 @@
+export class UserModel {
+  Id: string;
+  Location: string;
+  AddressOne: {};
+  AddressTwo: string;
+  City: string;
+  State: string;
+  PostalCode: string;
+  Country: string;
+  Email: string;
+  Name: {};
+  First: string;
+  Middle: string;
+  Last: string;
+  Gender: string;
+  Type: string;
+}
+
+
+
