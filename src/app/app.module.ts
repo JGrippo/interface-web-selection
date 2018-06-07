8b6f6e4b3128f1dc142f1ee08dae11996864a1f6
YouKnow232 <chaseharland@bellsouth.net>
2018-06-07 12:09:17 -0400

    Cleaned up leftovers [#158037410]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RoomListComponent } from './room-list/room-list.component';
 // test
 
 @NgModule({
   declarations: [
-    AppComponent,
-	RoomListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
