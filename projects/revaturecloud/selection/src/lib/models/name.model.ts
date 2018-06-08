7f2ec94f27f229f6981fb83e1344071d09cbc5ee
YouKnow232 <chaseharland@bellsouth.net>
2018-06-08 10:00:16 -0400

    Commented the rest of the model classes [#158037410]
    Format based on the documentation for roomAssociation and searchParamters.

--- projects/revaturecloud/selection/src/lib/models/name.model.ts
+++ projects/revaturecloud/selection/src/lib/models/name.model.ts
@@ -1,4 +1,17 @@
-// Subject to change
+///  <summary>
+///    An object representing the Name field in the
+///    User object returned in the housing selection API.
+///  </summary>
+///  <remarks>
+///    Endpoints which return this object as a nested object:
+///    "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
+///      (GET) All Users - /Users
+///      (GET) All Users Without A Room - /Users/Unassigned
+///
+///    The service-hub-housing-wiki's page on this object:
+///    "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/User-Model"
+///      Name - Custom class First - string, Middle - string, Last - string
+///  </remarks>
 
 export class Name {
   First: string;
