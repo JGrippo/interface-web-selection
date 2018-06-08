e0f96f096d7def9759fc53940d7d840724ad5cff
Donald Clements <silveranstavern@yahoo.com>
2018-06-08 04:02:45 -0400

    Added xml comments and additional service functionality to get a complex request of rooms, get all rooms, get all users, get all unassigned users.

--- projects/revaturecloud/selection/src/lib/selection.service.ts
+++ projects/revaturecloud/selection/src/lib/selection.service.ts
@@ -1,10 +1,21 @@
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+///  <summary>
+///    HTTP service for intaracting with RESTful housing selection service.
+///  </summary>
+///  <seealso cref="https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"/>
+///  <remarks>
+///    As the housing selection service is itself a work in progress, this file is subject to change as
+///    the structure of the API this service consumes also changes.
+///  </remarks>
+
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { RoomAssociation } from './models/roomAssociation.model';
-
+import { SearchParameters } from './models/searchParameters.model';
+import { Room } from './models/room.model';
+import { UserModel } from './models/user.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +26,8 @@ export class SelectionService {
   readonly apiEpRemoveFromRoom = '/Room/Remove';
   readonly apiEpBatches = '/Batches';
   readonly apiEpRooms = '/Rooms';
+  readonly apiEpUsers = '/Users';
+  readonly apiEpUnassignedUsers = '/Users/Unassigned';
   readonly apiEpRoomsComplexRequest = '/Rooms/ComplexObject'
 
   constructor(private http: HttpClient) { }
@@ -34,18 +47,54 @@ export class SelectionService {
       );
   }
 
-  // getAllRooms(queryString: ComplexObjectFilterComponent,
-  //   onSuccess,
-  //   onFail) {
-  //   const req = this.http.get(this.rootUrl + '/Rooms?' + queryString.complexObject());
-  //   const promise = req.toPromise();
+  ///  <summary>
+  ///    Gets collection of rooms that satisfy the specified search parameters.
+  ///  </summary>
+  getComplexRequestOfRooms(searchParameters: SearchParameters): Observable<Room[]> {
+    return this.http.get<Room[]>(this.rootUrl + this.apiEpRoomsComplexRequest,
+      { params: this.convertSearchParsObjToParams(searchParameters) })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
 
-  //   promise.then(
-  //     onSuccess,
-  //     onFail
-  //   );
-  // }
+  ///  <summary>
+  ///    Gets all rooms.
+  ///  </summary>
+  getAllRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(this.rootUrl + this.apiEpRooms)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
 
+  ///  <summary>
+  ///    Gets all unassigned users.
+  ///  </summary>
+  getAllUnassignedUsers(): Observable<UserModel[]> {
+    return this.http.get<UserModel[]>(this.rootUrl + this.apiEpUnassignedUsers)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+
+  ///  <summary>
+  ///    Gets all users.
+  ///  </summary>
+  getAllUsers(): Observable<UserModel[]> {
+    return this.http.get<UserModel[]>(this.rootUrl + this.apiEpUsers)
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+
+  ///  <summary>
+  ///    Initial template for basic common error handling.
+  ///  </summary>
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
@@ -61,6 +110,27 @@ export class SelectionService {
     return throwError(
       'Something bad happened; please try again later.');
   }; //TODO ensure method is fully applicable to use case.
+
+  ///  <summary>
+  ///    Method to convert Search SearchParameters to HttpParams.
+  ///  </summary>
+  ///  <seealso cref="https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"/>
+  ///  <remarks>
+  ///    According to the documentation in the see also section, it seems as if the API endpoint
+  ///    “/Rooms/ComplexObject” almost expects a Json object to be passed in the body of a get
+  ///    request, which is something that is not supported.  This may change in the future, but to
+  ///    accommodate this behavior this method has been created to convert such an object to search
+  ///    parameters that will be passed along with the request.
+  ///  </remarks>
+
+  private convertSearchParsObjToParams(searchParameters: SearchParameters) {
+    return new HttpParams()
+      .set("Batch", searchParameters.Batch)
+      .set("BatchMinimumPercentage", searchParameters.BatchMinimumPercentage.toString())
+      .set("Gender", searchParameters.Gender)
+      .set("IsCompletelyUnassigned", searchParameters.IsCompletelyUnassigned.toString())
+      .set("Location", searchParameters.Location);
+  };
 
   methodTestEx() {
     console.log('has been called');
