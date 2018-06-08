9f1473849b763932c3cf01d9a219efabfa9b69dc
Ram Adhikari <ramadhikari96@gmail.com>
2018-06-08 09:48:19 -0400

    Not for pull request

--- projects/revaturecloud/selection/src/lib/selection.module.ts
+++ projects/revaturecloud/selection/src/lib/selection.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SelectionComponent } from './selection.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 @NgModule({
   imports: [
+    HttpClientModule
   ],
   declarations: [SelectionComponent],
   exports: [SelectionComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RoomListComponent } from './room-list/room-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+
+
 // test
 
 @NgModule({
   declarations: [
     AppComponent,
-	RoomListComponent
+	  RoomListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
