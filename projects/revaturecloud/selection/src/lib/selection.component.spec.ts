7546bc5797ac93d70e7b8386b4a4d47b423c860d
Keenan Baynard <ksbaynard@hotmail.com>
2018-06-10 13:24:53 -0400

    implement service in component and component tests [#158036958]

--- projects/revaturecloud/selection/src/lib/selection.component.spec.ts
+++ projects/revaturecloud/selection/src/lib/selection.component.spec.ts
@@ -3,30 +3,108 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectionComponent } from './selection.component';
 import { SelectionService } from './selection.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Room } from './models/room.model';
+import { Address } from './models/address.model';
+import { Observable, of } from 'rxjs';
+import { Input } from '@angular/core';
+import { By } from 'protractor';
+import { RoomAssociation } from './models/roomAssociation.model';
+import { SearchParameters } from './models/searchParameters.model';
 
 describe('SelectionComponent', () => {
   let component: SelectionComponent;
+  let componentwith: SelectionComponent;
   let fixture: ComponentFixture<SelectionComponent>;
+  let Rooms;
+  let mockService;
+  let roomAssociations;
+  let users;
+  let searchParameters: SearchParameters;
 
-  beforeEach(async(() => {
+
+  beforeEach(() => {
+
+    roomAssociations = [
+      {
+        UserId: '1',
+        RoomId: 'guid1'
+      }
+    ];
+
+    searchParameters = {
+      Location: 'test',
+      Batch: 'test',
+      BatchMinimumPercentage: 2,
+      Gender: 'm',
+      IsCompletelyUnassigned: true
+    };
+
+    const testAddress1: Address = {
+      Street: 'Street',
+      AptNum: '314',
+      City: 'Tampa',
+      State: 'FL',
+      PostalCode: '33612',
+      Country: 'USA'
+    };
+
+    const testAddress2: Address = {
+      Street: 'Street',
+      AptNum: '315',
+      City: 'Tampa',
+      State: 'FL',
+      PostalCode: '33612',
+      Country: 'USA'
+    };
+
+    Rooms = [
+      {
+        RoomId: 'guid1',
+        Location: 'Tampa',
+        Address: testAddress1,
+        Vacancy: 4,
+        Occupancy: 4,
+        Gender: 'm'
+      },
+      {
+        RoomId: 'guid2',
+        Location: 'Tampa',
+        Address: testAddress2,
+        Vacancy: 1,
+        Occupancy: 4,
+        Gender: 'f'
+      }
+    ];
+
+    users = [
+      {
+        Id: '1',
+        Location: 'test',
+        Address: testAddress1,
+        Email: 'test@test.com',
+        Name: 'name',
+        Gener: 'm',
+        Type: 'test'
+      }
+    ];
+
+    mockService = jasmine.createSpyObj(
+      ['getAllRooms', 'addUserToRoom', 'getAllUsers', 'removeUserFromRoom',
+        'getComplexRequestOfRooms', 'getAllUnassignedUsers']
+    );
+
     TestBed.configureTestingModule({
       declarations: [
         SelectionComponent
       ],
       providers: [
-        SelectionService
-      ],
-      imports: [
-        HttpClientTestingModule
+        { provide: SelectionService, useValue: mockService }
       ]
-    })
-      .compileComponents();
-  }));
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SelectionComponent);
+    componentwith = new SelectionComponent(mockService);
     component = fixture.componentInstance;
-    // fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -44,21 +122,73 @@ describe('SelectionComponent', () => {
 
   it('should call getRooms on call', () => {
     const service = TestBed.get(SelectionService);
-    const spy = spyOn(component, 'getRooms');
+    const spy = spyOn(component, 'getAllRoomsComp');
 
-    component.getRooms();
+    component.getAllRoomsComp();
 
     fixture.detectChanges();
 
     expect(spy).toHaveBeenCalled();
   });
 
+  it('should get all rooms', () => {
+    mockService.getAllRooms.and.returnValue(of(Rooms));
+
+    componentwith.getAllRoomsComp();
+
+    expect(componentwith.rooms.length).toBe(2);
+  });
+
+  it('should get all users', () => {
+    mockService.getAllUsers.and.returnValue(of(users));
+
+    componentwith.getAllUsersComp();
+
+    expect(componentwith.users.length).toBe(1);
+  });
+
+  it('should call SelectionService.addUserToRoom with param when component addUserToRoom is called',
+    () => {
+      mockService.addUserToRoom.and.returnValue(of(roomAssociations));
+
+      componentwith.addUserToRoomComp(roomAssociations[0]);
+
+      expect(mockService.addUserToRoom).toHaveBeenCalledWith(roomAssociations[0]);
+
+    });
+
+  it('should call SelectionService.removeUserFromRoom with param when component removeUserFromRoomComp is called',
+    () => {
+      mockService.removeUserFromRoom.and.returnValue(of(roomAssociations));
+
+      componentwith.removeUserFromRoomComp(roomAssociations[0]);
+
+      expect(mockService.removeUserFromRoom).toHaveBeenCalledWith(roomAssociations[0]);
+    });
+
+    it('should get rooms of Complex Request', () => {
+      mockService.getComplexRequestOfRooms.and.returnValue(of(Rooms));
+
+      componentwith.getComplexRequestOfRoomsComp(searchParameters);
+
+      expect(mockService.getComplexRequestOfRooms).toHaveBeenCalledWith(searchParameters);
+      expect(componentwith.rooms.length).toBe(2);
+    });
+
+    it('should get all unassigned users ', () => {
+      mockService.getAllUnassignedUsers.and.returnValue(of(users));
+
+      componentwith.getAllUnassignedUsersComp();
+
+      expect(componentwith.users.length).toBe(1);
+    });
+
   // it('should call methodTestEx on initialization', () => {
-    //     let service = TestBed.get(SelectionService);
-    //     let spy = spyOn(service, 'methodTestEx');
+  //     let service = TestBed.get(SelectionService);
+  //     let spy = spyOn(service, 'methodTestEx');
 
-    //     fixture.detectChanges();
+  //     fixture.detectChanges();
 
-    //     expect(spy).toHaveBeenCalled();
-    //   });
+  //     expect(spy).toHaveBeenCalled();
+  //   });
 });
--- projects/revaturecloud/selection/src/lib/selection.component.ts
+++ projects/revaturecloud/selection/src/lib/selection.component.ts
@@ -1,9 +1,17 @@
+/// <summary>
+///     makes use of SelectionService by calling methods by either setting
+///     retrieved data to component fields
+///     or sending data to api through service
+/// </summary>
+
 import { Component, OnInit } from '@angular/core';
 import { SelectionService } from '../public_api';
 import { Room } from './models/room.model';
 import { Observable } from 'rxjs';
 import { RoomAssociation } from './models/roomAssociation.model';
 import { Response, ResponseOptions } from '@angular/http';
+import { SearchParameters } from './models/searchParameters.model';
+import { UserModel } from './models/user.model';
 
 @Component({
   selector: 'lib-selection',
@@ -12,6 +20,7 @@ import { Response, ResponseOptions } from '@angular/http';
 })
 export class SelectionComponent implements OnInit {
   rooms: Room[];
+  users: UserModel[];
 
   constructor(private service: SelectionService) { }
 
@@ -20,23 +29,59 @@ export class SelectionComponent implements OnInit {
 
 
   /// <summary>
-  ///     calls getAllRooms from service and sets component rooms to retrieved data\
+  ///     calls getAllRooms from service and sets component rooms to retrieved data
   /// </summary>
-  getRooms(): void {
+  getAllRoomsComp(): void {
     this.service.getAllRooms().subscribe((data: Room[]) =>
       this.rooms = data,    // sets rooms to retrieved data
-      (err: any) => console.log(err),    // logs errors
+      (err: any) => console.log('Error Status ' + err.status + ', Error : ' + err),    // logs errors
       () => console.log('Retrieved Rooms'));    // logs success
   }
 
-  addUserToRoom(roomAssociation: RoomAssociation): void {
+  /// <summary>
+  ///     adds user to room via roomAssociation (UserId, RoomId)
+  /// </summary>
+  addUserToRoomComp(roomAssociation: RoomAssociation): void {
     this.service.addUserToRoom(roomAssociation).subscribe((response: Response) =>
       (err: any) => console.log('Error Status' + err.status));
   }
 
-  removeUserFromRoom(roomAssociation: RoomAssociation): void {
+  /// <summary>
+  ///     removes user from room via roomAssociation (UserId, RoomId)
+  /// </summary>
+  removeUserFromRoomComp(roomAssociation: RoomAssociation): void {
     this.service.removeUserFromRoom(roomAssociation).subscribe((response: Response) =>
       (err: any) => console.log('Error Status' + err.status));
+  }
+
+  /// <summary>
+  ///     gets list of rooms via ComplexRequest search query (Location, Batch, BatchMinimumPercentage, Gender, IsCompletelyUnassigned)
+  /// </summary>
+  getComplexRequestOfRoomsComp(searchParameters: SearchParameters): void {
+    this.service.getComplexRequestOfRooms(searchParameters).subscribe((data: Room[]) =>
+      this.rooms = data,
+      (err: any) => console.log('Error Status ' + err.status + ', Error : ' + err),
+      () => console.log('Retrieved Rooms'));
+  }
+
+  /// <summary>
+  ///      calls getAllUnassignedUsers from service and sets component users to retrieved data
+  /// </summary>
+  getAllUnassignedUsersComp(): void {
+    this.service.getAllUnassignedUsers().subscribe((data: UserModel[]) =>
+      this.users = data,
+      (err: any) => console.log('Error Status' + err.status + ', Error : ' + err),
+      () => console.log('Retrieved Users'));
+  }
+
+  /// <summary>
+  ///      calls getAllUsers from service and sets component users to retrieved data
+  /// </summary>
+  getAllUsersComp(): void {
+    this.service.getAllUsers().subscribe((data: UserModel[]) =>
+      this.users = data,
+      (err: any) => console.log('Error Status ' + err.status + ', Error : ' + err),
+      () => console.log('Retrieved Users'));
   }
 
 }
