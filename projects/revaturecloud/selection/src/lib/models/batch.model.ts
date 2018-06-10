5e2b89434f8d5edfe77af4cd7485c688e2dd131c
unknown <chaseharland@bellsouth.net>
2018-06-10 13:05:37 -0400

    Reformatted comments to JSDoc [#158037410]

--- projects/revaturecloud/selection/src/lib/models/batch.model.ts
+++ projects/revaturecloud/selection/src/lib/models/batch.model.ts
@@ -1,15 +1,14 @@
-///  <summary>
-///    An object representing the expected output from the
-///    housing selection API endpoint "/Batches/".
-///  </summary>
-///  <remarks>
-///    Endpoints which return this object:
-///    "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
-///      (GET) All Batches - /Batches
-///
-///    The service-hub-housing-wiki's page on this object:
-///    "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Batch-Model"
-///  </remarks>
+/**
+* An object representing the expected output from the
+* housing selection API endpoint "/Batches/".
+* 
+* Endpoints which return this object:
+* "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
+*   (GET) All Batches - /Batches
+* 
+* The service-hub-housing-wiki's page on this object:
+* "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Batch-Model"
+*/
 
 export class Batch {
   BatchId: string;
--- projects/revaturecloud/selection/src/lib/models/roomAssociation.model.ts
+++ projects/revaturecloud/selection/src/lib/models/roomAssociation.model.ts
@@ -1,13 +1,12 @@
-/// <summary>
-///   This is the model for room associations, used for both adding and
-///   removing someone from a room.
-/// </summary>
-/// <remarks>
-///   Because there is more than one method that utilize this same data model,
-///   it has been extracted into its own model class instead of being an
-///   anonymous object such that there is a single place where I can go to
-///   update the model for the change.
-/// </remarks>
+/**
+* This is the model for room associations, used for both adding and
+* removing someone from a room.
+* 
+* Because there is more than one method that utilize this same data model,
+* it has been extracted into its own model class instead of being an
+* anonymous object such that there is a single place where I can go to
+* update the model for the change.
+*/
 
 export class RoomAssociation {
   UserId: string;
