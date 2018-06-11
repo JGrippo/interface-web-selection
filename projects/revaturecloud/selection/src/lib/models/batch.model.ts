34f2ea549ba56172c04d9baebdb3f262d6b197d4
YouKnow232 <chaseharland@bellsouth.net>
2018-06-11 11:25:33 -0400

    Updated models to sync with ServiceHub team [#158037410]

--- projects/revaturecloud/selection/src/lib/models/batch.model.ts
+++ projects/revaturecloud/selection/src/lib/models/batch.model.ts
@@ -1,20 +1,25 @@
 /**
 * An object representing the expected output from the
 * housing selection API endpoint "/Batches/".
-* 
+*
 * Endpoints which return this object:
 * "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
 *   (GET) All Batches - /Batches
-* 
+*
 * The service-hub-housing-wiki's page on this object:
 * "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Batch-Model"
 */
+import { User } from "./user.model";
+import { Address } from "./address.model";
 
 export class Batch {
-  batchId: string;
+  id: string;
   startDate: string;
   endDate: string;
   batchName: string;
   batchOccupancy: number;
   batchSkill: string;
+
+  users: User[];
+  address: Address;
 }
--- projects/revaturecloud/selection/src/lib/models/room.model.ts
+++ projects/revaturecloud/selection/src/lib/models/room.model.ts
@@ -1,7 +1,7 @@
 /**
 * An object representing the expected output from the
 * housing selection API endpoints "/Rooms/" and "/Rooms/ComplexObject".
-* 
+*
 * Endpoints which return this object:
 * "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
 *   (GET) All Rooms - /Rooms
@@ -9,18 +9,21 @@
 *   (GET) All Rooms With Unassigned Bed By Gender - /Rooms/ComplexObject
 *   (GET) All Rooms With All Unassigned Bed By Gender - /Rooms/ComplexObject
 *   (GET) All Rooms With Unassigned Bed By Gender/Batch Percentage - /Rooms/ComplexObject
-* 
+*
 * The service-hub-housing-wiki's page on this object:
 * "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Room-Model"
 */
 
 import { Address } from "./address.model";
+import { User } from "./user.model";
 
 export class Room {
   roomId: string;
   location: string;
-  address: Address;
   vacancy: number;
   occupancy: number;
   gender: string;
+
+  address: Address;
+  users: User[];
 }
