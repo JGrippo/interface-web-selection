390ee48a8e7545e6dc329c1da2cb98d63e731d30
YouKnow232 <chaseharland@bellsouth.net>
2018-06-11 16:54:29 -0400

    Added two unit tests [#158037410]

--- projects/revaturecloud/selection/src/lib/services/filter.service.spec.ts
+++ projects/revaturecloud/selection/src/lib/services/filter.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { FilterService } from './filter.service';
+import { SearchParameters } from "../models/searchParameters.model";
 
 describe('FilterService', () => {
   beforeEach(() => {
@@ -11,5 +12,33 @@ describe('FilterService', () => {
 
   it('should be created', inject([FilterService], (service: FilterService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should update on setFilter', inject([FilterService], (service: FilterService) => {
+    //Arrange
+    let filter: SearchParameters = {
+      location: "1",
+      batch: "2",
+      batchMinimumPercentage: 10,
+      gender: "3",
+      isCompletelyUnassigned: false,
+    }
+
+    //Subscribe
+    let filterActual: SearchParameters;
+    service.getFilter().subscribe((f) => filterActual = f);
+
+    //Set
+    service.setFilter(filter);
+
+    //Expect
+    expect(filterActual).toBe(filter);
+  }));
+
+  it('should return null initially', inject([FilterService], (service: FilterService) => {
+    let filterActual: SearchParameters;
+    service.getFilter().subscribe((f) => filterActual = f);
+
+    expect(filterActual).toBeFalsy();
   }));
 });
