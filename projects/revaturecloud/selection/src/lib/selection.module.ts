7aa9dc633fce637838cdb12744bd0e3986d9f6ce
YouKnow232 <chaseharland@bellsouth.net>
2018-06-12 15:22:51 -0400

    Added some imports to selection.module [#158037410]

--- projects/revaturecloud/selection/src/lib/selection.module.ts
+++ projects/revaturecloud/selection/src/lib/selection.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SelectionComponent } from './selection.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FilterPanelComponent } from "./components/filter-panel/filter-panel.component";
@@ -6,6 +8,8 @@ import { FilterService } from "./services/filter.service";
 @NgModule({
   imports: [
     HttpClientModule,
+    FormsModule,
+    CommonModule,
   ],
   declarations: [
     SelectionComponent,
