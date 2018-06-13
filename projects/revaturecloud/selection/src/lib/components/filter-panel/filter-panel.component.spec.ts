f08584ccc63436c871c36c25895ec6ea3f5d5dd1
YouKnow232 <chaseharland@bellsouth.net>
2018-06-13 12:13:49 -0400

    Added filter panel jasmine tests [#158037410]

--- projects/revaturecloud/selection/src/lib/components/filter-panel/filter-panel.component.spec.ts
+++ projects/revaturecloud/selection/src/lib/components/filter-panel/filter-panel.component.spec.ts
@@ -1,25 +1,70 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilterPanelComponent } from './filter-panel.component';
+import { FilterService } from '../../services/filter.service';
+import { FilterSortService } from '../../services/filter-sort.service';
 
 describe('FilterPanelComponent', () => {
   let component: FilterPanelComponent;
   let fixture: ComponentFixture<FilterPanelComponent>;
+  let componentwith: FilterPanelComponent;
+  let MockFilter;
+  let MockFilterSort;
 
   beforeEach(async(() => {
+    MockFilter = jasmine.createSpyObj(['getFilter', 'setFilter']);
+    MockFilterSort = jasmine.createSpyObj(['getFilter', 'setFilter']);
+
     TestBed.configureTestingModule({
-      declarations: [ FilterPanelComponent ]
+      declarations: [ FilterPanelComponent ],
+      providers: [
+        {provide: FilterService, useValue: MockFilter},
+        {provide: FilterSortService, userValue: MockFilterSort}
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FilterPanelComponent);
+    componentwith = new FilterPanelComponent(null, MockFilter, MockFilter);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should reset properly', () => {
+    component.batch = "batch";
+    component.city = "asdf";
+    component.building = "asdf";
+
+    component.reset();
+
+    expect(component.batch).toBeNull();
+    expect(component.city).toBeNull();
+    expect(component.building).toBeNull();
+  });
+
+  it('should publish to filter service', () => {
+    let setSpy = spyOn(MockFilter, 'setFilter');
+
+    componentwith.batch = 'batch1';
+
+    componentwith.submit();
+
+    expect(setSpy).toHaveBeenCalledWith(componentwith.filter);
+  });
+
+  it('should publish to filter sort service', () => {
+    let setSpy = spyOn(MockFilterSort, 'setFilter');
+
+    componentwith.sortByMostVacancies = true;
+
+    componentwith.submit();
+
+    expect(setSpy).toHaveBeenCalledWith(componentwith.sort);
   });
 });
