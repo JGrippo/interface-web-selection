ae8b90dd9683cad310981246b94cff69b952b1ca
Ram Adhikari <ramadhikari96@gmail.com>
2018-06-13 18:45:06 -0400

    Merged user-wrapper with 158037410 and it works.

--- projects/revaturecloud/selection/src/lib/selection.module.ts
+++ projects/revaturecloud/selection/src/lib/selection.module.ts
@@ -1,44 +1,47 @@
 import { CommonModule } from "@angular/common";
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SelectionComponent } from './selection.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UserWrapperComponent } from './components/user-wrapper/user-wrapper.component';
-import { SearchPipe } from './shared/search.pipe';
 import { FilterPanelComponent } from "./components/filter-panel/filter-panel.component";
 import { FilterService } from "./services/filter.service";
 import { UserStore } from './stores/user.store';
+import { RoomStore } from './stores/room.store';
+
+import { UserWrapperComponent } from './components/user-wrapper/user-wrapper.component';
+import { SearchPipe } from './shared/search.pipe';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { UserCardComponent } from './components/user-card/user-card.component';
-import { RoomStore } from './stores/room.store';
+
 
 
 @NgModule({
+  declarations: [
+    SelectionComponent,
+    FilterPanelComponent,
+    UserWrapperComponent,
+    SearchPipe,
+    UserCardComponent
+  ],
+
   imports: [
     HttpClientModule,
+    FormsModule,
+    CommonModule,
     MatPaginatorModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatGridListModule,
-    FormsModule,
-    CommonModule,
+    MatGridListModule
   ],
-  declarations: [
-    SelectionComponent,
-    UserWrapperComponent,
-    SearchPipe,
-    UserCardComponent,
-    FilterPanelComponent
-  ],
+
   providers: [
     FilterService,
     UserStore,
     RoomStore
   ],
-
   exports: [SelectionComponent]
 })
 export class SelectionModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { UserWrapperComponent } from '../../projects/revaturecloud/selection/src/lib/components/user-wrapper/user-wrapper.component';
-import { SearchPipe } from '../../projects/revaturecloud/selection/src/lib/shared/search.pipe';
 import { SelectionModule } from "../../projects/revaturecloud/selection/src/lib/selection.module";
-// test
+
 
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { UserCardComponent } from 'projects/revaturecloud/selection/src/lib/components/user-card/user-card.component';
-import { SelectionComponent } from 'projects/revaturecloud/selection/src/public_api';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SelectionComponent,
-    UserWrapperComponent,
-    SearchPipe,
-    UserCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -32,11 +25,11 @@ import { SelectionComponent } from 'projects/revaturecloud/selection/src/public_
     FormsModule,
     HttpModule,
     RouterModule,
+    SelectionModule,
     BrowserAnimationsModule,
     MatPaginatorModule,
     MatCardModule,
-    MatGridListModule,
-    SelectionModule
+    MatGridListModule
   ],
   bootstrap: [AppComponent]
 })
