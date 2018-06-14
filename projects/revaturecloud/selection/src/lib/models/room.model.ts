f334123f4f14f5cfb00885fd6cee84301a95507f
Donald Clements <silveranstavern@yahoo.com>
2018-06-14 12:40:29 -0400

    Model Documentation formatting and addtions. [#158036958]

--- projects/revaturecloud/selection/src/lib/models/room.model.ts
+++ projects/revaturecloud/selection/src/lib/models/room.model.ts
@@ -1,21 +1,25 @@
 /**
-* An object representing the expected output from the
-* housing selection API endpoints "/Rooms/" and "/Rooms/ComplexObject".
-*
-* Endpoints which return this object:
-* "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
-*   (GET) All Rooms - /Rooms
-*   (GET) All Rooms in One Location - /Rooms/ComplexObject
-*   (GET) All Rooms With Unassigned Bed By Gender - /Rooms/ComplexObject
-*   (GET) All Rooms With All Unassigned Bed By Gender - /Rooms/ComplexObject
-*   (GET) All Rooms With Unassigned Bed By Gender/Batch Percentage - /Rooms/ComplexObject
-*
-* The service-hub-housing-wiki's page on this object:
-* "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Room-Model"
-*/
+ * An object representing the expected output from the
+ * housing selection API endpoints "/Rooms/" and "/Rooms/ComplexObject".
+ *
+ * Endpoints which return this object:
+ * "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Housing-Selection-API-Endpionts"
+ *   (GET) All Rooms - /Rooms
+ *   (GET) All Rooms in One Location - /Rooms/ComplexObject
+ *   (GET) All Rooms With Unassigned Bed By Gender - /Rooms/ComplexObject
+ *   (GET) All Rooms With All Unassigned Bed By Gender - /Rooms/ComplexObject
+ *   (GET) All Rooms With Unassigned Bed By Gender/Batch Percentage - /Rooms/ComplexObject
+ *
+ * The service-hub-housing-wiki's page on this object:
+ * "https://github.com/mjbradvica/service-hub-housing-ui-wiki/wiki/Room-Model"
+ * @export
+ * @class Room
+ */
 
-import { Address } from "./address.model";
-//import { User } from "./user.model";
+import {
+  Address
+} from './address.model';
+
 
 export class Room {
   roomId: string;
@@ -25,5 +29,5 @@ export class Room {
   gender: string;
 
   address: Address;
-  //users: User[];
+
 }
