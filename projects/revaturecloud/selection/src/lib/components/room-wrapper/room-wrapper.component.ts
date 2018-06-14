ea495135e26dce0db89f965179e64b5040e60815
Keenan Baynard <ksbaynard@hotmail.com>
2018-06-14 09:28:20 -0400

    update room wrapper [#158036958]

--- projects/revaturecloud/selection/src/lib/components/room-wrapper/room-wrapper.component.ts
+++ projects/revaturecloud/selection/src/lib/components/room-wrapper/room-wrapper.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy, PipeTransform, Pipe } from '@angular/core';
 import { Room } from '../../models/room.model';
 import { Address } from '../../models/address.model';
-import { MatPaginator, MatTableDataSource, MatSort, PageEvent } from '@angular/material';
+import { MatPaginator, PageEvent } from '@angular/material';
 import { Observable } from 'rxjs';
 import { RoomStore } from '../../stores/room.store';
 
@@ -12,14 +12,8 @@ import { RoomStore } from '../../stores/room.store';
   styleUrls: ['./room-wrapper.component.css']
 })
 export class RoomWrapperComponent implements OnInit, OnDestroy {
-  displayedColumns = ['roomId', 'location', 'address', 'vacancy', 'occupancy', 'gender'];
-  dataSource: MatTableDataSource<Room>;
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   rooms: Room[];
-  roomsPage: Room[];
   searchRooms = '';
   pageEvent: PageEvent;
   pageOptions = [4, 8, 12];
@@ -32,24 +26,11 @@ export class RoomWrapperComponent implements OnInit, OnDestroy {
       .subscribe((rooms: Room[]) => {
         this.rooms = rooms;
       });
-    this.roomsPage = this.rooms;
-  }
-
-  updateArray() {
-    let start = this.pageEvent.pageSize * this.pageEvent.pageIndex;
-    let end;
-    if (start + this.pageEvent.pageSize > this.pageEvent.length) {
-      end = this.pageEvent.length;
-    } else {
-      end = start + this.pageEvent.pageSize;
-    }
-    this.roomsPage = this.rooms.slice(start, end);
   }
 
   ngOnDestroy() {
   }
 }
-
 
 @Pipe({
   name: 'roomSearch',
