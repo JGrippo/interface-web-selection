41f90ec583740db995bd39d2c1234537fabbdb9f
Ram Adhikari <ramadhikari96@gmail.com>
2018-06-14 10:03:49 -0400

    Modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import { SelectionModule } from "../../projects/revaturecloud/selection/src/lib/selection.module";
 
-
 @NgModule({
   declarations: [
     AppComponent
