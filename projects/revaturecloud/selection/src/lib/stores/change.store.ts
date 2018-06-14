fe2e0fcb34877ddce0f8de443bc88765c7c0ee29
Donald Clements <silveranstavern@yahoo.com>
2018-06-14 11:26:49 -0400

    Added comments to changes.store.ts [#158036958]

--- projects/revaturecloud/selection/src/lib/stores/change.store.ts
+++ projects/revaturecloud/selection/src/lib/stores/change.store.ts
@@ -3,17 +3,29 @@ import { BehaviorSubject } from 'rxjs';
 import { Tracker } from '../models/tracker.model';
 import { User } from '../models/user.model';
 import { Room } from '../models/room.model';
-
+/**
+ * Store to keep track of changes for the current session.
+ *
+ * @export
+ * @class ChangeStore
+ */
 @Injectable({
   providedIn: 'root'
 })
+
 export class ChangeStore {
 
   private _changeTracker: BehaviorSubject<Tracker[]> = new BehaviorSubject<Tracker[]>([]);
   private _changeArray: Tracker[];
   constructor() {
   }
-
+  /**
+   * Method that adds User, Room, and Action as "Add" to store, and updates
+   * observable for those subscribed.
+   * @param {User} user
+   * @param {Room} room
+   * @memberof ChangeStore
+   */
   addUserToRoom(user: User, room: Room) {
     let tracker: Tracker;
     tracker.User = user;
@@ -22,6 +34,13 @@ export class ChangeStore {
     this._changeArray.push(tracker);
     this._changeTracker.next(this._changeArray);
   }
+  /**
+   * Method that adds User, Room, and Action as "Remove" to store, and updates
+   * observable for those subscribed.
+   * @param {User} user
+   * @param {Room} room
+   * @memberof ChangeStore
+   */
   removeUserFromRoomComp(user: User, room: Room) {
     let tracker: Tracker;
     tracker.User = user;
@@ -30,11 +49,16 @@ export class ChangeStore {
     this._changeArray.push(tracker);
     this._changeTracker.next(this._changeArray);
   }
+  /**
+   * Clears all changes for current session.
+   *
+   * @memberof ChangeStore
+   */
   clearTrackedChanges() {
     this._changeArray = [];
     this._changeTracker.next(this._changeArray);
   }
-  get changes() {
+  get changes$() {
     return this._changeTracker.asObservable();
   }
 }
