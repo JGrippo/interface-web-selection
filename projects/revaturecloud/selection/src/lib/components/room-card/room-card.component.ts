791f3a7a0b093d8918a51c307f37e2c95f06a167
Keenan Baynard <ksbaynard@hotmail.com>
2018-06-15 10:47:53 -0400

    update

--- projects/revaturecloud/selection/src/lib/components/room-card/room-card.component.ts
+++ projects/revaturecloud/selection/src/lib/components/room-card/room-card.component.ts
@@ -16,27 +16,10 @@ import { UserStore } from '../../stores/user.store';
 export class RoomCardComponent implements OnInit {
 
   @Input() room: Room;
-  users: User[];
 
-  constructor(private userStore: UserStore) {  }
+  constructor() {  }
 
   ngOnInit() {
-    this.userStore.users.subscribe((data: any) => {
-      this.users = data;
-    });
-    this.getUsers(this.room.roomId);
-    this.users = this.users.slice(0,4);
-  }
-
-  getUsers(id: string) {
-    // this.users = this.users.filter( (usr:User) => {
-    //     if(usr && usr.room) {
-    //       return usr.room.roomId === id;
-    //     }
-    //     else {
-    //       return false;
-    //     }
-    //   });
   }
 
   ngOnDestroy() {
