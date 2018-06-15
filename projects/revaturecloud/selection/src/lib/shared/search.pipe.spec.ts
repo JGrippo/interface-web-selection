54be0862652ccf2848e7b0a5db6b4109a3fc2c92
Ram Adhikari <ramadhikari96@gmail.com>
2018-06-15 15:15:24 -0400

    Test added for pipe

--- projects/revaturecloud/selection/src/lib/shared/search.pipe.spec.ts
+++ projects/revaturecloud/selection/src/lib/shared/search.pipe.spec.ts
@@ -1,9 +1,80 @@
 import { SearchPipe } from './search.pipe';
+import { User } from '../models/user.model';
 
-describe('SearchPipe', () => {
-  it('should find the inputs', () => {
-    let searchString = new SearchPipe();
+
+describe('Pipe: SearchPipe', () => {
+  it('should find the input string', () => {
+    let searchPipe = new SearchPipe();
+    let testUsers: User[] = [
+      {
+        id: '10',
+        location: 'Reston',
+        email: "a@a.com",
+        gender: "male",
+        type: "Known",
+        name: {
+          id: "1000",
+          first: "Petty",
+          middle: "Meagan",
+          last: "Barber"
+        },
+        address: {
+          address1: "Fowler",
+          address2: "411",
+          city: "Reston",
+          state: "Virginia",
+          postalCode: "43417",
+          country: "USA"
+        }
+      },
+      {
+        id: "11",
+        location: "Tampa",
+        email: "b@b.com",
+        gender: "male",
+        type: "Unknown",
+        name: {
+          id: "1001",
+          first: "Bridget",
+          middle: "Erica",
+          last: "Padilla"
+        },
+        address: {
+          address1: "Fowler",
+          address2: "249",
+          city: "Tampa",
+          state: "Florida",
+          postalCode: "88080",
+          country: "USA"
+        }
+      },
+      {
+        id: "12",
+        location: "Tampa",
+        email: "c@c.com",
+        gender: "male",
+        type: "Unknown",
+        name: {
+          id: "1002",
+          first: "Mullins",
+          middle: "Maude",
+          last: "Wynn"
+        },
+        address: {
+          address1: "Livingston",
+          address2: "122",
+          city: "Tampa",
+          state: "Virginia",
+          postalCode: "92287",
+          country: "USA"
+        }
+      }
+    ]
+
+    expect(searchPipe.transform(testUsers[0], 'Petty').toEqual(testUsers[0]));
   });
+
+
 });
 
 
