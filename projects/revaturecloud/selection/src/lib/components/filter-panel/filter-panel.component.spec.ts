5d8a08a8ba382358d5730f15cdf82b547bc1a29b
YouKnow232 <chaseharland@bellsouth.net>
2018-06-19 16:47:53 -0400

    Fixed Filter Panel unit tests [#158037410]

--- projects/revaturecloud/selection/src/lib/components/filter-panel/filter-panel.component.spec.ts
+++ projects/revaturecloud/selection/src/lib/components/filter-panel/filter-panel.component.spec.ts
@@ -9,32 +9,70 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { FilterPanelComponent } from './filter-panel.component';
 import { FilterService } from '../../services/filter.service';
 import { FilterSortService } from '../../services/filter-sort.service';
+import { PutService } from '../../services/put.service';
 import { UserStore } from '../../stores/user.store';
 import { RoomStore } from '../../stores/room.store';
 import { BatchStore } from '../../stores/batch.store';
 import { LocationStore } from '../../stores/location.store';
+import { ChangeStore } from '../../stores/change.store';
 import { SearchParameters } from '../../models/searchParameters.model';
 import { SortParameters } from '../../models/sortParameters.model';
 import { Batch } from '../../models/batch.model';
 import { Room } from '../../models/room.model';
 
+class MockUserStore {
+  updateUsers(): void {}
+}
+class MockRoomStore {
+  updateRooms(): void {}
+  get rooms(): Observable<Room[]> {
+    return new BehaviorSubject<Room[]>([]).asObservable();
+  }
+}
+class MockBatchStore {
+  updateBatches(): void {}
+  get batches(): Observable<Batch[]> {
+    return new BehaviorSubject<Batch[]>([]).asObservable();
+  }
+}
+class MockLocationStore {
+  get locations(): Observable<Room[]> {
+    return new BehaviorSubject<Room[]>([]).asObservable();
+  }
+}
+class MockFilterService {
+  setFilter(params: SearchParameters): void {}
+}
+class MockFilterSortService {
+  setFilter(params: SortParameters): void {}
+}
+
 describe('FilterPanelComponent', () => {
   let component: FilterPanelComponent;
   let fixture: ComponentFixture<FilterPanelComponent>;
-  let mockFilSer;
-  let mockFilSorSer;
-  let mockUserStore;
-  let mockRoomStore;
-  let mockBatchStore;
-  let mockLocStore;
+  // let mockFilSer;
+  // let mockFilSorSer;
+  // let mockUserStore;
+  // let mockRoomStore;
+  // let mockBatchStore;
+  // let mockLocStore;
+  let filSer: FilterService;
+  let filSorSer: FilterSortService;
+  let userStore: UserStore;
+  let roomStore: RoomStore;
+  let batchStore: BatchStore;
+  let locStore: LocationStore;
 
   beforeEach(async(() => {
-    let mockFilSer = jasmine.createSpyObj('FilterService', ['setFilter', 'getFilter']);
-    let mockFilSorSer = jasmine.createSpyObj('FilterSortService', ['setFilter']);
-    let mockUserStore = jasmine.createSpyObj('UserStore', ['']);
-    let mockRoomStore = jasmine.createSpyObj('RoomStore', ['']);
-    let mockBatchStore = jasmine.createSpyObj('BatchStore', ['']);
-    let mockLocStore = jasmine.createSpyObj('LocationStore', ['']);
+    // let mockFilSer = jasmine.createSpyObj('FilterService', ['setFilter']);
+    // let mockFilSorSer = jasmine.createSpyObj('FilterSortService', ['setFilter']);
+    // let mockUserStore = jasmine.createSpyObj('UserStore', ['updateUsers']);
+    // let mockRoomStore = jasmine.createSpyObj('RoomStore', ['updateRooms']);
+    // let mockBatchStore = jasmine.createSpyObj('BatchStore', ['updateBatches', 'batches']);
+    // let mockLocStore = jasmine.createSpyObj('LocationStore', ['locations']);
+    // filSer = new FilterService();
+    // filSorSer = new FilterSortService();
+    // userStore = new UserStore();
 
     TestBed.configureTestingModule({
       imports: [
@@ -45,12 +83,12 @@ describe('FilterPanelComponent', () => {
       ],
       declarations: [ FilterPanelComponent ],
       providers: [
-        {provide: FilterService, usevalue: mockFilSer},
-        {provide: FilterSortService, usevalue: mockFilSorSer},
-        {provide: UserStore, usevalue: mockUserStore},
-        {provide: RoomStore, usevalue: mockRoomStore},
-        {provide: BatchStore, usevalue: mockBatchStore},
-        {provide: LocationStore, usevalue: mockLocStore}
+        {provide: FilterService, useClass: MockFilterService},
+        {provide: FilterSortService, useClass: MockFilterSortService},
+        {provide: UserStore, useClass: MockUserStore},
+        {provide: RoomStore, useClass: MockRoomStore},
+        {provide: BatchStore, useClass: MockBatchStore},
+        {provide: LocationStore, useClass: MockLocationStore}
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
@@ -72,7 +110,7 @@ describe('FilterPanelComponent', () => {
     let expectedLocation = "Location";
     let expectedGender = "male";
     let expectedVRO = true;
-    let expectedHBA = false;
+    let expectedHBA = true;
     let expectedAssigned = true;
 
     component.batchId = expectedBatchId;
@@ -93,29 +131,36 @@ describe('FilterPanelComponent', () => {
   });
 
   it('should publish to filter service', () => {
+    let filterSer = TestBed.get(FilterService);
+    let setFilterSpy = spyOn(filterSer, 'setFilter');
     component.batchId = '1002';
 
     component.update();
 
-    expect(TestBed.get(MockFilterService).setFilter).toHaveBeenCalledWith(component.filter);
+    expect(setFilterSpy).toHaveBeenCalledWith(component.filter);
   });
 
   it('should publish to sort filter serivce', () => {
     let expectSortByMV = true;
+    let setFilterSpy = spyOn(TestBed.get(FilterSortService), 'setFilter');
 
     component.sortByMostVacancies = expectSortByMV;
 
     component.updateSort();
 
     expect(component.sort.sortByMostVacancies).toBe(expectSortByMV);
-    expect(TestBed.get(MockFilterSortService).setFilter).toHaveBeenCalledWith(component.sort);
+    expect(setFilterSpy).toHaveBeenCalledWith(component.sort);
   });
 
   it('should update the stores', () => {
+    let userUpdateSpy = spyOn(TestBed.get(UserStore), 'updateUsers');
+    let roomUpdateSpy = spyOn(TestBed.get(RoomStore), 'updateRooms');
+    let batcUpdateSpy = spyOn(TestBed.get(BatchStore), 'updateBatches');
+
     component.update();
 
-    expect(TestBed.get(MockUserStore).updateUsers).toHaveBeenCalled();
-    expect(TestBed.get(MockRoomStore).updateRooms).toHaveBeenCalled();
-    expect(TestBed.get(MockBatchStore).updateBatches).toHaveBeenCalled();
+    expect(userUpdateSpy).toHaveBeenCalled();
+    expect(roomUpdateSpy).toHaveBeenCalled();
+    expect(batcUpdateSpy).toHaveBeenCalled();
   });
 });
