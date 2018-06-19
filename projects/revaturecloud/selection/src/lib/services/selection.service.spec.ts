508f7fd4c233dc88b14e7f210b43a0fd237fd480
Donald Clements <silveranstavern@yahoo.com>
2018-06-19 14:57:53 -0400

    Added more tests to selection.service.spec.ts.  [#158037410]

--- projects/revaturecloud/selection/src/lib/services/selection.service.spec.ts
+++ projects/revaturecloud/selection/src/lib/services/selection.service.spec.ts
@@ -14,10 +14,15 @@ describe('SelectionService Tests', () => {
 
   let selectionService: SelectionService;
   let httpTestingController: HttpTestingController;
-  const s: SearchParameters = new SearchParameters {
-    batch= null, location= 'Tampa', gender= 'M',
-    batchMinimumPercentage= null, isCompletelyUnassigned= null,
-    hasBedAvailable= null, assigned= null};
+  const filter: SearchParameters = {
+    batch: null,
+    location: null,
+    gender: null,
+    batchMinimumPercentage: null,
+    isCompletelyUnassigned: null,
+    hasBedAvailable: null,
+    assigned: null
+  };
   beforeEach(() => {
 
     TestBed.configureTestingModule({
@@ -60,6 +65,45 @@ describe('SelectionService Tests', () => {
   });
   it('should GET all batches', () => {
     selectionService.getAllBatches()
+      .subscribe((data: Batch[]) => {
+        expect(data.length).toBe(6);
+      });
+
+    // tslint:disable-next-line:prefer-const
+    let batchesRequest: TestRequest = httpTestingController.expectOne(selectionService.rootUrl + selectionService.apiEpBatches);
+    expect(batchesRequest.request.method).toEqual('GET');
+
+    batchesRequest.flush(mockBatches);
+  });
+
+  // Complex request tests
+  it('should GET all rooms with search params', () => {
+    selectionService.getComplexRequestOfRooms(filter)
+      .subscribe((data: Room[]) => {
+        expect(data.length).toBe(2);
+      });
+
+    // tslint:disable-next-line:prefer-const
+    let roomsRequest: TestRequest = httpTestingController.expectOne(selectionService.rootUrl + selectionService.apiEpRooms);
+    expect(roomsRequest.request.method).toEqual('GET');
+
+    roomsRequest.flush(mockRooms);
+  });
+
+  it('should GET all users with search params', () => {
+    selectionService.getComplexRequestOfUsers(filter)
+      .subscribe((data: User[]) => {
+        expect(data.length).toBe(4);
+      });
+
+    // tslint:disable-next-line:prefer-const
+    let usersRequest: TestRequest = httpTestingController.expectOne(selectionService.rootUrl + selectionService.apiEpUsers);
+    expect(usersRequest.request.method).toEqual('GET');
+
+    usersRequest.flush(mockUsers);
+  });
+  it('should GET all batches with search params', () => {
+    selectionService.getComplexRequestOfBatches(filter)
       .subscribe((data: Batch[]) => {
         expect(data.length).toBe(6);
       });
